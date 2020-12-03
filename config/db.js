55bda0eaf8330a6b7603f63eb43011a0de8fb323
Natthanon Prateeppanyapat <natthanon.prat@gmail.com>
2020-12-03 18:01:12 +0700

    change config data to PROD on heroku

--- config/db.js
+++ config/db.js
@@ -1,11 +1,28 @@
 const pg = require('pg');
+
+//LOCAL
+// const USER = "postgres";
+// const HOST = "127.0.0.1";
+// const DATABASE = "postgres";
+// const PASSWORD = "1234";
+// const PORT = "5432";
+
+//PROD
+const USER = "ashqsgtuqrhtss";
+const HOST = "ec2-54-236-122-55.compute-1.amazonaws.com";
+const DATABASE = "d5t8apequasnj2";
+const PASSWORD = "fc9ff56871cf66280eecad085e957a45df181e87b98c6328f0be6b0c9416912c";
+const PORT = "5432";
+
+
 const pool = new pg.Pool({
-    user: 'postgres',
-    host: '127.0.0.1',
-    database: 'postgres',
-    password: '1234',
-    port: '5432'
+    user: USER,
+    host: HOST,
+    database: DATABASE,
+    password: PASSWORD,
+    port: PORT
 });
 
 module.exports = pool;
     
+
